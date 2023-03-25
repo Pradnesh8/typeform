60298eeaf5e73d483c7492cf6f421c1b2c0fab00
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-25 22:48:10 +0530

    Basic components

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Question from './Question'
+
+const Body = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            <Question />
+        </div>
+
+    )
+}
+
+export default Body
