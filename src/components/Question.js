9650d4d07d4351277c8aa7185e6ae9c47b764361
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-04-03 01:30:34 +0530

    Toggle Option Multi select option functionality

--- src/components/Question.js
+++ src/components/Question.js
@@ -285,13 +285,22 @@ const OptionQuestion = () => {
         }
     }, [qno]);
     const handleSelect = (roleName) => {
-        const form = formData
-        form["role"] = roleName;
-        setFormData({ ...formData, ...form });
-        console.log("ss", Math.round(qno + 1 / 7 * 100));
-        setProgress(Math.round(4 / 7 * 100));
-        setError("");
-        setRole(roleName);
+        if (roleName === role) {
+            const form = formData
+            delete form['role']
+            setFormData({ ...formData, ...form });
+            console.log("ss", Math.round(qno + 1 / 7 * 100));
+            setProgress(Math.round(3 / 7 * 100));
+            setRole("");
+        } else {
+            const form = formData
+            form["role"] = roleName;
+            setFormData({ ...formData, ...form });
+            console.log("ss", Math.round(qno + 1 / 7 * 100));
+            setProgress(Math.round(4 / 7 * 100));
+            setError("");
+            setRole(roleName);
+        }
     }
     const changeOtherRole = (event) => {
         if (otherRole.length === 0) {
@@ -384,8 +393,10 @@ const OptionQuestion = () => {
 const MultiOptionQuestion = () => {
     const { qno, setQno, formData, setFormData } = useContext(QuestionContext);
     const { setProgress } = useContext(AppContext);
+    const [error, setError] = useState("");
     const [animate, setAnimate] = useState(false);
     const [goals, setGoals] = useState([]);
+    const [selGoals, setSelGoals] = useState([]);
     const alpha = ['A', 'B', 'C', 'D', 'E'];
     const RoleGoals = {
         "Founder or CXO": [
@@ -409,6 +420,36 @@ const MultiOptionQuestion = () => {
             clearTimeout(i);
         }
     }, [qno]);
+    const handleSelect = (_goal) => {
+        const form = formData
+        if (form["goals"] && form["goals"].includes(_goal)) {
+            if (form['goals'].includes(_goal)) {
+                form['goals'] = form['goals'].filter(g => g !== _goal);
+                if (form['goals'].length === 0) {
+                    delete form['goals']
+                    setSelGoals([]);
+                } else {
+                    setSelGoals(form['goals']);
+                }
+            }
+            setFormData({ ...formData, ...form });
+            console.log("ss", Math.round(qno + 1 / 7 * 100));
+            setProgress(Math.round(4 / 7 * 100));
+            setError("");
+        } else {
+            if (form["goals"]?.length === 2) {
+                setError("Only 2 options can be selected");
+            } else {
+                const form = formData
+                form["goals"] ? form["goals"].push(_goal) : form["goals"] = [_goal];
+                setFormData({ ...formData, ...form });
+                console.log("ss", Math.round(qno + 1 / 7 * 100));
+                form["goals"].length === 2 ? setProgress(Math.round(5 / 7 * 100)) : setProgress(Math.round(4 / 7 * 100));
+                setError("");
+                setSelGoals(form["goals"]);
+            }
+        }
+    }
     const getGoals = () => {
         if (formData['role'] === 'Founder or CXO') {
             setGoals(RoleGoals['Founder or CXO']);
@@ -430,19 +471,26 @@ const MultiOptionQuestion = () => {
                                 <span className='qno-text'>5 →</span>
                                 <span>Johb, what's your professional goal for the next 12 months? *</span>
                             </div>
-                            <div className="multi-drop-selected">
-                                Choose 2
-                            </div>
+                            {
+                                selGoals.length != 2 &&
+                                <div className="multi-drop-selected">
+                                    Choose {2 - selGoals.length}
+                                </div>
+                            }
                             <div className='options multi-options'>
                                 {
                                     goals.map((goal, index) => {
-                                        return <div className='role-opt'>
+                                        return <div className={formData["goals"]?.includes(goal) ? 'role-opt selected-option' : 'role-opt'} onClick={() => handleSelect(goal)}>
                                             <span className='role-opt-btn-key'>{alpha[index]}</span>
                                             <span className='role-opt-text'>{goal}</span>
                                         </div>
                                     })
                                 }
                             </div>
+                            {
+                                (error.length > 0 && goals.length >= 0) &&
+                                <ErrorAlert errMsg={error} />
+                            }
                         </section>
                         <section className='actions'>
                             <button className='cta-btn' onClick={() => setQno(prevQue => prevQue + 1)}>OK ✓</button>
